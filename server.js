f465a54d8bb3c8fce1fac343a1daf7c303ceb256
Morgan Fogarty <mofo37@gmail.com>
2017-04-24 16:54:41 -0700

    made server run with express

---
+++ server.js
@@ -0,0 +1,14 @@
+const app = require('./app');
+const http = require('http');
+const DB_URI = 'mongodb://localhost:27017/puppies';
+
+const connection = require('./connect');
+connection.connect(DB_URI);
+
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log('server running on', server.address());
+});
+
+
